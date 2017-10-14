f10320168316a5d34b9159d700d2daa42ecf2ac8
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-10-14 02:39:16 -0500

    Parsing now looks at entire webpage

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ from html.parser import HTMLParser
 import urllib.request
 import time
 
+# Useful for working with dynamically generated html
+from bs4 import BeautifulSoup
+from selenium import webdriver
 tags = ["h3"]
 ids = ["headline"]
 
@@ -30,18 +33,12 @@ class MyHTMLFilter(HTMLParser):
 print("Attempting to open URL")
 #url = "https://www.nytimes.com/2017/10/06/us/las-vegas-shooting.html"
 url = "https://query.nytimes.com/search/sitesearch/?action=click&contentCollection=U.S.&region=TopBar&WT.nav=searchWidget&module=SearchSubmit&pgtype=article#/puppies"
-fp = urllib.request.urlopen(url)
-print("URL opened. Waiting 5 secs before reading.")
-time.sleep(5)
 
-print("Attempting to read page.")
+# Working with dynamically generated html
+browser = webdriver.PhantomJS(executable_path=r"/Users/twalen/Desktop/phantomjs-2.1.1-macosx/bin/phantomjs")
+browser.get(url)
+html = browser.page_source
+soup = BeautifulSoup(html, "html.parser")
+a = soup.find_all('h3')
 
-mybytes = fp.read()
-
-
-mystr = mybytes.decode("utf8")
-fp.close()
-
-print("Page read. Attempting to parse")
-parser = MyHTMLFilter()
-parser.feed(mystr)
+print(a)
